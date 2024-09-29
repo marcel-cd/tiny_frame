4ce8361a571681f3b396b3b11a541fc0af271e13
globi <globi@apple.local>
2024-09-29 16:45:46 +0200

    Adding clean example code

--- example/example.dart
+++ example/example.dart
@@ -2,20 +2,17 @@ import 'dart:typed_data';
 
 import 'package:tiny_frame/tiny_frame.dart';
 
-void write(Uint8List data) => print('write: $data');
 void main() {
-  const mainID = 1;
-  // create a TinyFrame instance as master (peer id set)
-  // the write method has to be implemented, tinyframe will call it
-  // to send the data after building the frame
-  final tf = TinyFrame(write);
+  const typeID = 1;
+  // create a TinyFrame instance with a write function
+  // TF_WriteImpl in C Library
+  final tf = TinyFrame((Uint8List data) => print('write: $data'));
   // some optional settings
   tf.cksumType = 'xor';
   // generate a listener for the id 1
-  tf.addIdListener(mainID, (Uint8List frame) => print('main: $frame'));
+  tf.addTypeListener(typeID, (_, TF_Msg msg) => print('main: $msg'));
   // if no listener is found for the id, this listener will be called
-  tf.addFallbackListener((Uint8List frame) => print('fallback: $frame'));
-
+  tf.addFallbackListener((_, TF_Msg msg) => print('fallback: $msg'));
   // send to slave on id 1
-  tf.send(mainID, Uint8List.fromList('abcde'.codeUnits));
+  tf.send(typeID, Uint8List.fromList('abcde'.codeUnits));
 }
